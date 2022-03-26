4426bb549316acb66cb3fadf2bf8652c51c5b4e8
Colin G Fitzhenry <cgfitzhenry@gmail.com>
2022-03-25 23:51:11 -0400

    sequelize database file done

--- database/index.js
+++ database/index.js
@@ -1,27 +1,60 @@
-const mysql = require('mysql');
+const { Sequelize, DataTypes } =  require('sequelize');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'student',
-  password: 'student',
-  database: 'YOUR_DATABASE_NAME_HERE'
-});
+// create database in SQL shell
 
-connection.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Connected to MySQL!')
+// Connect db // new instance of Sequelize
+const sequelize = new Sequelize('cowlist', 'root', null, {
+  host: "localhost",
+  dialect: "mysql"
+})
+
+// Test Connection
+sequelize
+  .authenticate() // tests connection
+  .then(() => {
+    console.log('Connected!')
+  })
+  .catch(err => console.error(err))
+
+
+// Define Model
+const Cows = sequelize.define('Cows', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  des: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
-});
-
-// Your Database Queries Here!!
+})
 
 
+// Create Tables
+sequelize
+  .sync() // tests connection
+  .then(() => {
+    console.log('Synced!')
+  })
+  .catch(err => console.error(err))
 
 
+// Define Methods / Functions
+let save = (param) => {
+  Cows.create({
+    name: param.name
+    des: param.des
+  })
+}
 
-// Don't forget to export your functions!
-module.exports = {
+let getAll = () => {
+  return Cows.findAll()
+}
 
-};
+module.exports save;
+module.exports getAll;
