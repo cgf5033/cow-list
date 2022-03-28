bf464e7843866ec6386acdaebea74c47c5dc3e4d
Colin G Fitzhenry <cgfitzhenry@gmail.com>
2022-03-27 22:15:05 -0400

    back end working

--- database/index.js
+++ database/index.js
@@ -2,23 +2,16 @@ const { Sequelize, DataTypes } =  require('sequelize');
 
 // create database in SQL shell
 
+
 // Connect db // new instance of Sequelize
-const sequelize = new Sequelize('cowlist', 'root', null, {
+const db = new Sequelize('cowlist', 'root', null, {
   host: "localhost",
   dialect: "mysql"
 })
 
-// Test Connection
-sequelize
-  .authenticate() // tests connection
-  .then(() => {
-    console.log('Connected!')
-  })
-  .catch(err => console.error(err))
-
 
 // Define Model
-const Cows = sequelize.define('Cows', {
+const Cow = db.define('cow', {
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
@@ -32,29 +25,40 @@ const Cows = sequelize.define('Cows', {
     type: DataTypes.STRING,
     allowNull: false
   }
-})
+});
 
-
-// Create Tables
-sequelize
-  .sync() // tests connection
-  .then(() => {
-    console.log('Synced!')
-  })
+// Test Connection
+db.authenticate() // connect/login to sequ to database with credentials above
+  .then(db.sync()) // take schema from db.define  // { force: true } -> drops database after you save a file in vs-code
   .catch(err => console.error(err))
 
 
 // Define Methods / Functions
-let save = (param) => {
-  Cows.create({
-    name: param.name
-    des: param.des
+// Create:
+const save = (obj) => {
+  return Cow.create(obj)
+}
+
+// Read:
+const getAll = () => {
+  return Cow.findAll()
+}
+
+// Update: NOT FINISHED -- CHECK!!!! { id: 'id', name: 'name', des: 'des' }
+const update = ({ id, name, des }) => {
+  //console.log({ id, name, des });
+  // Cow.update({ name, des }, { where: { id }})
+}
+
+// Delete:
+const remove = (obj) => {
+  return Cow.destroy({
+    where: obj
   })
 }
 
-let getAll = () => {
-  return Cows.findAll()
-}
-
-module.exports save;
-module.exports getAll;
+module.exports = { save, getAll, update, remove };
+// module.exports = save;
+// module.exports = getAll;
+// module.exports = update;
+// module.exports = remove;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const {save, getAll, update, remove} = require('../database')
 
 const PORT = 3000;
 const app = express();
 
+// Middleware
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev')); // FORGOT -- NEED THIS
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+
+// Routes
+app.get('/api/cows', (req, res) => {
+  getAll()
+  .then(result => res.status(200).send(result)) // distinguish 'result'
+  .catch(err => console.error(err))
+})
+
+app.post('/api/cows', (req, res) => {
+  //console.log(req.body)
+  save(req.body)
+    .then(() => res.status(201).send('posted to app.post!'))
+    .catch(err => console.error(err))
+})
+
+// app.put('/api/cows', (req, res) => {
+//   console.log('PUT says hello');
+//   console.log(req.body);
+//   //res.send('GOOOD');
+//   update(req.body)
+//     .then((result) => res.status(201).send(result))
+//     .catch(err => res.status(500).send(err))
+// })
+
+app.delete('/api/cows', (req, res) => {
+  remove(req.body)
+    .then(() => res.status(200).send('Got a DELETE request at /api/cows!'))
+    .catch(err => console.error(err))
 })
 
 app.listen(PORT, () => {
